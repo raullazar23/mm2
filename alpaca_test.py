5d60337ec6711297d8ecd8f658e84e91b5c388db
Raul <raullazar23@gmail.com>
2025-03-08 19:16:17 +0200

    refactor symbol: input

--- alpaca_test.py
+++ alpaca_test.py
@@ -7,27 +7,28 @@ import time
 stream = alpaca_client.StockDataStream(API_KEY, API_SECRET)
 price_history = []
 vwap_history = []
+symbol = "BTC/USD"
 
 
 async def handle_trade(trade):
     try:
-        alpaca_client.get_position("FLUT")
+        positions = alpaca_client.get_position(symbol)
     except Exception as e:
         global price_history, vwap_history
-        print("No position in FLUT")
+        print("No position in ", symbol)
         # Store price history
         price_history.append(trade.bid_price)
         if len(price_history) > buy_strategy.ema_long_window:
             price_history = price_history[-buy_strategy.ema_long_window:]
 
         # Update VWAP history
-        vwap_history.append(buy_strategy.calculate_vwap("FLUT"))
+        vwap_history.append(buy_strategy.calculate_vwap(symbol))
         if len(vwap_history) > 10:
             vwap_history = vwap_history[-10:]
-            if buy_strategy.check_buy_conditions(trade.bid_price, "FLUT", price_history, vwap_history):
+            if buy_strategy.check_buy_conditions(trade.bid_price, symbol, price_history, vwap_history):
                 try:
-                    alpaca_client.place_order("FLUT", 3, OrderSide.BUY)
-                    print("Bought 3 shares of FLUT")
+                    alpaca_client.place_order(symbol, 3, OrderSide.BUY)
+                    print("Bought 3 shares of ", symbol)
                     time.sleep(2)
                 except Exception as e:
                     print(e)
@@ -37,18 +38,19 @@ async def handle_trade(trade):
             print("Not enough data for buy conditions")
         return
     
-    positions = alpaca_client.get_position("FLUT")
+    positions = alpaca_client.get_position(symbol)
     print(positions.avg_entry_price, trade.bid_price)
     if (trade.bid_price - float(positions.avg_entry_price) >= 0.25):
         try:
-            alpaca_client.place_order("FLUT", positions.qty, OrderSide.SELL)
+            alpaca_client.place_order(symbol, positions.qty, OrderSide.SELL)
             print ("Succsessful trade")
+            time.sleep(2)
         except Exception as e:
             print(e)
 
 
 
 
-stream.subscribe_quotes(handle_trade, "FLUT")
+stream.subscribe_quotes(handle_trade, symbol)
 runner = stream.run()
 
