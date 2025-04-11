8d328ee2ceaf077f6be8ef4571ddca5fd287d4b1
Raul <raullazar23@gmail.com>
2025-04-11 20:33:54 +0300

    added check for trading hours, refactor code

--- utils.py
+++ utils.py
@@ -1,32 +1,7 @@
-from datetime import datetime
-import pytz
+from datetime import datetime, timedelta
 import time
 import json
-
-# Define Eastern Time Zone
-eastern = pytz.timezone("US/Eastern")
-
-# Get current time in Eastern Time
-now = datetime.now(eastern).time()
-
-# Define market open and close times
-market_open = datetime.strptime("09:30", "%H:%M").time()
-market_close = datetime.strptime("16:00", "%H:%M").time()
-
-
-def is_trading_hours():
-    if market_open <= now <= market_close:
-        return True
-    else:
-        return False
-    
-
-def check_trading_hours():
-    print("Verifying trading hours")
-    if not is_trading_hours():
-        print("Not trading hours")
-        time.sleep(600)
-        return
+import asyncio
     
 # Load JSON config
 with open("symbols_quant.json", "r") as file:
@@ -37,3 +12,21 @@ stocks = config["stocks"]
 # Function to get quantity by symbol
 def get_quantity(symbol):
     return stocks.get(symbol, "Symbol not found")
+
+def check_trading_hours(client):
+    clock = client.get_clock()
+    now = clock.timestamp
+    timeframe = (clock.next_open - clock.next_close).total_seconds()
+    if clock.is_open:
+        today_open = (clock.next_close - now).total_seconds()
+        if (timeframe - today_open) <= 1800:
+            return True
+    return False
+
+def is_last_half_hour_trade_day(client):
+    clock = client.get_clock()
+    now = clock.timestamp
+    if clock.is_open:
+        last_half_hour = (now - clock.next_close).total_seconds() <= 900 # 15 minutes
+        return last_half_hour
+    return False
